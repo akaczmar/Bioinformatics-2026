a955dc50dcf72c19104c782950a5378aa47f8694
akaczmar <123973795+akaczmar@users.noreply.github.com>
2026-02-05 15:08:56 -0500

    Add files via upload

---
+++ learning_bash.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+#Andy Kaczmar Assignment 1
+
+#Part 1
+greeting=Hello
+your_name=Andy
+echo $greeting $your_name!
+echo "This script is helping you learn bash."
+
+#Part 2
+mkdir /home/arkaczmar/Desktop/bioinformatics
+mkdir /home/arkaczmar/Desktop/bioinformatics/bash_scripts
+
+#Part 3
+mv /home/arkaczmar/Desktop/bash_template.txt /home/arkaczmar/Desktop/bioinformatics/bash_scripts
+
+#Part 4
+mv /home/arkaczmar/Desktop/bioinformatics/bash_scripts/bash_template.txt /home/arkaczmar/Desktop/bioinformatics/bash_scripts/bash_template.sh
